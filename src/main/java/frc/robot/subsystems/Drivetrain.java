382b98fd6a30595245aece3fc783de0bfe59781c
Jacob Hotz <lordgaming1010@gmail.com>
2023-10-13 13:20:19 -0700

    Fix Encoders to be spark max

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -4,190 +4,223 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.ADIS16470_IMU;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.Constants.DriveConstants;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drivetrain extends SubsystemBase {
-  // The motors on the left side of the drive.
-  private final MotorControllerGroup leftMotors =
-      new MotorControllerGroup(
-          new PWMSparkMax(DriveConstants.LEFT_MOTOR1_PORT),
-          new PWMSparkMax(DriveConstants.LEFT_MOTOR2_PORT));
+    // The motors on the left side of the drive.
+    private final CANSparkMax leftMotor1 = new CANSparkMax(DriveConstants.LEFT_MOTOR1_PORT, MotorType.kBrushless);
+    private final CANSparkMax leftMotor2 = new CANSparkMax(DriveConstants.LEFT_MOTOR2_PORT, MotorType.kBrushless);
+    private final CANSparkMax rightMotor1 = new CANSparkMax(DriveConstants.RIGHT_MOTOR1_PORT, MotorType.kBrushless);
+    private final CANSparkMax rightMotor2 = new CANSparkMax(DriveConstants.RIGHT_MOTOR2_PORT, MotorType.kBrushless);
 
-  // The motors on the right side of the drive.
-  private final MotorControllerGroup rightMotors =
-      new MotorControllerGroup(
-          new PWMSparkMax(DriveConstants.RIGHT_MOTOR1_PORT),
-          new PWMSparkMax(DriveConstants.RIGHT_MOTOR2_PORT));
+    private final MotorControllerGroup leftMotors = new MotorControllerGroup(
+            leftMotor1,
+            leftMotor2);
 
-  // The robot's drive
-  private final DifferentialDrive drive = new DifferentialDrive(leftMotors, rightMotors);
+    // The motors on the right side of the drive.
+    private final MotorControllerGroup rightMotors = new MotorControllerGroup(
+            rightMotor1,
+            rightMotor2);
 
-  // The left-side drive encoder
-  private final Encoder leftEncoder =
-      new Encoder(
-          DriveConstants.LEFT_ENCODER_PORT[0],
-          DriveConstants.LEFT_ENCODER_PORT[1],
-          DriveConstants.LEFT_ENCODER_REVERSED);
+    // The robot's drive
+    private final DifferentialDrive drive = new DifferentialDrive(leftMotors, rightMotors);
 
-  // The right-side drive encoder
-  private final Encoder rightEncoder =
-      new Encoder(
-          DriveConstants.RIGHT_ENCODER_PORT[0],
-          DriveConstants.RIGHT_ENCODER_PORT[1],
-          DriveConstants.RIGHT_ENCODER_REVERSED);
+    // The left-side drive encoder
+    private final RelativeEncoder leftEncoder = leftMotor1.getEncoder();
 
-  // The gyro sensor
-  private final ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+    // The right-side drive encoder
+    private final RelativeEncoder rightEncoder = rightMotor1.getEncoder();
 
-  // Odometry class for tracking robot pose
-  private final DifferentialDriveOdometry odometry;
+    // The gyro sensor
+    private final ADIS16470_IMU gyro = new ADIS16470_IMU();
 
-  /** Creates a new DriveSubsystem. */
-  public Drivetrain() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    rightMotors.setInverted(true);
+    // Odometry class for tracking robot pose
+    private final DifferentialDriveOdometry odometry;
 
-    // Sets the distance per pulse for the encoders
-    leftEncoder.setDistancePerPulse(DriveConstants.ENCODER_DISTANCE_PER_PULSE);
-    rightEncoder.setDistancePerPulse(DriveConstants.ENCODER_DISTANCE_PER_PULSE);
+    /** Creates a new DriveSubsystem. */
+    public Drivetrain() {
+        // Define variables
 
-    resetEncoders();
-    odometry =
-        new DifferentialDriveOdometry(
-            gyro.getRotation2d(), leftEncoder.getDistance(), rightEncoder.getDistance());
-  }
+        // We need to invert one side of the drivetrain so that positive voltages
+        // result in both sides moving forward. Depending on how your robot's
+        // gearbox is constructed, you might have to invert the left side instead.
+        rightMotors.setInverted(true);
 
-  @Override
-  public void periodic() {
-    // Update the odometry in the periodic block
-    odometry.update(
-        gyro.getRotation2d(), leftEncoder.getDistance(), rightEncoder.getDistance());
-  }
+        // Sets the distance per pulse for the encoders
+        leftEncoder.setPositionConversionFactor(DriveConstants.ENCODER_POSITION_CONVERSION_FACTOR);
+        rightEncoder.setPositionConversionFactor(DriveConstants.ENCODER_POSITION_CONVERSION_FACTOR);
 
-  /**
-   * Returns the currently-estimated pose of the robot.
-   *
-   * @return The pose.
-   */
-  public Pose2d getPose() {
-    return odometry.getPoseMeters();
-  }
+        resetEncoders();
+        odometry = new DifferentialDriveOdometry(
+                getRotation2d(), leftEncoder.getPosition(), rightEncoder.getPosition());
+    }
 
-  /**
-   * Returns the current wheel speeds of the robot.
-   *
-   * @return The current wheel speeds.
-   */
-  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
-    return new DifferentialDriveWheelSpeeds(leftEncoder.getRate(), rightEncoder.getRate());
-  }
+    @Override
+    public void periodic() {
+        // Update the odometry in the periodic block
+        odometry.update(
+                getRotation2d(), leftEncoder.getPosition(), rightEncoder.getPosition());
+    }
 
-  /**
-   * Resets the odometry to the specified pose.
-   *
-   * @param pose The pose to which to set the odometry.
-   */
-  public void resetOdometry(Pose2d pose) {
-    resetEncoders();
-    odometry.resetPosition(
-        gyro.getRotation2d(), leftEncoder.getDistance(), rightEncoder.getDistance(), pose);
-  }
+    /**
+     * Returns the currently-estimated pose of the robot.
+     *
+     * @return The pose.
+     */
+    public Pose2d getPose() {
+        return odometry.getPoseMeters();
+    }
 
-  /**
-   * Drives the robot using arcade controls.
-   *
-   * @param fwd the commanded forward movement
-   * @param rot the commanded rotation
-   */
-  public void arcadeDrive(double fwd, double rot) {
-    drive.arcadeDrive(fwd, rot);
-  }
+    /**
+     * Returns the current wheel speeds of the robot.
+     *
+     * @return The current wheel speeds.
+     */
+    public DifferentialDriveWheelSpeeds getWheelSpeeds() {
+        return new DifferentialDriveWheelSpeeds(leftEncoder.getVelocity(), rightEncoder.getVelocity());
+    }
 
-  /**
-   * Controls the left and right sides of the drive directly with voltages.
-   *
-   * @param leftVolts the commanded left output
-   * @param rightVolts the commanded right output
-   */
-  public void tankDriveVolts(double leftVolts, double rightVolts) {
-    leftMotors.setVoltage(leftVolts);
-    rightMotors.setVoltage(rightVolts);
-    drive.feed();
-  }
+    /**
+     * Resets the odometry to the specified pose.
+     *
+     * @param pose The pose to which to set the odometry.
+     */
+    public void resetOdometry(Pose2d pose) {
+        resetEncoders();
+        odometry.resetPosition(
+                getRotation2d(), leftEncoder.getPosition(), rightEncoder.getPosition(), pose);
+    }
 
-  /** Resets the drive encoders to currently read a position of 0. */
-  public void resetEncoders() {
-    leftEncoder.reset();
-    rightEncoder.reset();
-  }
+    /**
+     * Drives the robot using arcade controls.
+     *
+     * @param fwd the commanded forward movement
+     * @param rot the commanded rotation
+     */
+    public void drive(double fwd, double rot) {
+        drive.arcadeDrive(fwd, rot);
+    }
 
-  /**
-   * Gets the average distance of the two encoders.
-   *
-   * @return the average of the two encoder readings
-   */
-  public double getAverageEncoderDistance() {
-    return (leftEncoder.getDistance() + rightEncoder.getDistance()) / 2.0;
-  }
+    /**
+     * Controls the left and right sides of the drive directly with voltages.
+     *
+     * @param leftVolts  the commanded left output
+     * @param rightVolts the commanded right output
+     */
+    public void tankDriveVolts(double leftVolts, double rightVolts) {
+        leftMotors.setVoltage(leftVolts);
+        rightMotors.setVoltage(rightVolts);
+        drive.feed();
+    }
 
-  /**
-   * Gets the left drive encoder.
-   *
-   * @return the left drive encoder
-   */
-  public Encoder getLeftEncoder() {
-    return leftEncoder;
-  }
+    /** Resets the drive encoders to currently read a position of 0. */
+    public void resetEncoders() {
+        leftEncoder.setPosition(0);
+        rightEncoder.setPosition(0);
+    }
 
-  /**
-   * Gets the right drive encoder.
-   *
-   * @return the right drive encoder
-   */
-  public Encoder getRightEncoder() {
-    return rightEncoder;
-  }
+    /**
+     * Gets the average distance of the two encoders.
+     *
+     * @return the average of the two encoder readings
+     */
+    public double getAverageEncoderDistance() {
+        return (leftEncoder.getPosition() + rightEncoder.getPosition()) / 2.0;
+    }
 
-  /**
-   * Sets the max output of the drive. Useful for scaling the drive to drive more slowly.
-   *
-   * @param maxOutput the maximum output to which the drive will be constrained
-   */
-  public void setMaxOutput(double maxOutput) {
-    drive.setMaxOutput(maxOutput);
-  }
+    /**
+     * Gets the left drive encoder.
+     *
+     * @return the left drive encoder
+     */
+    public RelativeEncoder getLeftEncoder() {
+        return leftEncoder;
+    }
 
-  /** Zeroes the heading of the robot. */
-  public void zeroHeading() {
-    gyro.reset();
-  }
+    /**
+     * Gets the right drive encoder.
+     *
+     * @return the right drive encoder
+     */
+    public RelativeEncoder getRightEncoder() {
+        return rightEncoder;
+    }
 
-  /**
-   * Returns the heading of the robot.
-   *
-   * @return the robot's heading in degrees, from -180 to 180
-   */
-  public double getHeading() {
-    return gyro.getRotation2d().getDegrees();
-  }
+    /**
+     * Sets the max output of the drive. Useful for scaling the drive to drive more
+     * slowly.
+     *
+     * @param maxOutput the maximum output to which the drive will be constrained
+     */
+    public void setMaxOutput(double maxOutput) {
+        drive.setMaxOutput(maxOutput);
+    }
 
-  /**
-   * Returns the turn rate of the robot.
-   *
-   * @return The turn rate of the robot, in degrees per second
-   */
-  public double getTurnRate() {
-    return -gyro.getRate();
-  }
+    /** Zeroes the heading of the robot. */
+    public void zeroHeading() {
+        gyro.reset();
+    }
+
+    /**
+     * Returns the heading of the robot.
+     * 
+     * @return a Rotation2d representing the robot's heading
+     */
+    public Rotation2d getRotation2d() {
+        return new Rotation2d(gyro.getAngle());
+    }
+
+    /**
+     * Returns the heading of the robot.
+     *
+     * @return the robot's heading in degrees, from -180 to 180
+     */
+    public double getHeading() {
+        return getRotation2d().getDegrees();
+    }
+
+    /**
+     * Returns the turn rate of the robot.
+     *
+     * @return The turn rate of the robot, in degrees per second
+     */
+    public double getTurnRate() {
+        return -gyro.getRate();
+    }
+
+    /**
+     * Schedules the config commands to run. This should be run every time the robot
+     * is disabled.
+     */
+    public void scheduleConfigCommands() {
+        CommandScheduler.getInstance().schedule(new InstantCommand(() -> {
+            leftMotor1.restoreFactoryDefaults();
+            leftMotor2.restoreFactoryDefaults();
+            rightMotor1.restoreFactoryDefaults();
+            rightMotor2.restoreFactoryDefaults();
+        }).ignoringDisable(true));
+    }
+
+    /**
+     * Resets the relative rotation encoders to currently read a position of 0.
+     */
+    public void resetRelativeRotationEncoders() {
+        CommandScheduler.getInstance().schedule(
+                new InstantCommand(() -> resetEncoders()).ignoringDisable(false));
+    }
+
 }
